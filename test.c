2e6a776a126fc002ef42f7d1df58e5a10fca5688
Dmitry Stogov <dmitry@zend.com>
2020-01-23 01:59:01 +0300

    Extend test_scale() with support for arrays

--- test.c
+++ test.c
@@ -51,6 +51,46 @@ PHP_FUNCTION(test_test2)
 }
 /* }}}*/
 
+static int do_scale(zval *return_value, zval *x, zend_long factor)
+{
+	if (Z_TYPE_P(x) == IS_LONG) {
+		RETVAL_LONG(Z_LVAL_P(x) * factor);
+	} else if (Z_TYPE_P(x) == IS_DOUBLE) {
+		RETVAL_DOUBLE(Z_DVAL_P(x) * factor);
+	} else if (Z_TYPE_P(x) == IS_STRING) {
+		zend_string *ret = zend_string_safe_alloc(Z_STRLEN_P(x), factor, 0, 0);
+		char *p = ZSTR_VAL(ret);
+
+		while (factor-- > 0) {
+			memcpy(p, Z_STRVAL_P(x), Z_STRLEN_P(x));
+			p += Z_STRLEN_P(x);
+		}
+		*p = '\000';
+		RETVAL_STR(ret);
+	} else if (Z_TYPE_P(x) == IS_ARRAY) {
+		zend_array *ret = zend_new_array(zend_array_count(Z_ARR_P(x)));
+		zend_ulong idx;
+		zend_string *key;
+		zval *val, tmp;
+
+		ZEND_HASH_FOREACH_KEY_VAL(Z_ARR_P(x), idx, key, val) {
+			if (do_scale(&tmp, val, factor) != SUCCESS) {
+				return FAILURE;
+			}
+			if (key) {
+				zend_hash_add(ret, key, &tmp);
+			} else {
+				zend_hash_index_add(ret, idx, &tmp);
+			}
+		} ZEND_HASH_FOREACH_END();
+		RETVAL_ARR(ret);
+	} else {
+		php_error_docref(NULL, E_WARNING, "unexpected argument type");
+		return FAILURE;
+	}
+	return SUCCESS;
+}
+
 PHP_FUNCTION(test_scale)
 {
 	zval *x;
@@ -62,24 +102,7 @@ PHP_FUNCTION(test_scale)
 		Z_PARAM_LONG(factor)
 	ZEND_PARSE_PARAMETERS_END();
 
-	if (Z_TYPE_P(x) == IS_LONG) {
-		RETURN_LONG(Z_LVAL_P(x) * factor);
-	} else if (Z_TYPE_P(x) == IS_DOUBLE) {
-		RETURN_DOUBLE(Z_DVAL_P(x) * factor);
-	} else if (Z_TYPE_P(x) == IS_STRING) {
-		zend_string *ret = zend_string_safe_alloc(Z_STRLEN_P(x), factor, 0, 0);
-		char *p = ZSTR_VAL(ret);
-
-		while (factor-- > 0) {
-			memcpy(p, Z_STRVAL_P(x), Z_STRLEN_P(x));
-			p += Z_STRLEN_P(x);
-		}
-		*p = '\000';
-		RETURN_STR(ret);
-	} else {
-		php_error_docref(NULL, E_WARNING, "unexpected argument type");
-		return;
-	}
+	do_scale(return_value, x, factor);
 }
 
 static PHP_GINIT_FUNCTION(test)
